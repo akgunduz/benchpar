5ba14a8bf0ec1f2ada55efe225360b8a6ff7ad3b
Haluk Akgunduz <akgunduz@gmail.com>
2015-09-21 17:55:30 +0300

    batch added

---
+++ batch/batch_c9.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Client/benchtool_c9 -a m -m 2 -o spr -r 10 1
+Client/benchtool_c9 -a m -m 2 -o spr -r 5 2
+Client/benchtool_c9 -a m -m 2 -o spr -r 3 3
+Client/benchtool_c9 -a c -m 1 -o spr -r 100 1
+Client/benchtool_c9 -a c -m 1 -o spr -r 100 2
+Client/benchtool_c9 -a c -m 1 -o spr -r 100 3
+Client/benchtool_c9 -a c -m 1 -o spr -r 100 4
+Client/benchtool_c9 -a s -m 0 -o spr -r 10000 1
+Client/benchtool_c9 -a s -m 0 -o spr -r 10000 2
+Client/benchtool_c9 -a s -m 0 -o spr -r 10000 3
